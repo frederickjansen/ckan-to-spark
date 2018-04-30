ca01cae78756fbf242c8a4e1238f0055ef0cda1f
Frederick Jansen <frederick.jansen@gmail.com>
2018-04-30 14:21:14 -0400

    Add AnalysisError exception to parquet conversion Workaround for invalid header names

--- ckan_to_spark/helpers/spark.py
+++ ckan_to_spark/helpers/spark.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.utils import AnalysisException
 import conf
 import os
 
@@ -10,3 +11,5 @@ def csv_to_parquet(file):
         df.write.mode('overwrite').parquet(os.path.join(conf.FLAGS.output_dir, os.path.splitext(file)[0]) + '.parquet')
     except TypeError:
         print('Spark Error: ' + file)
+    except AnalysisException as e:
+        print('Spark Analysis Error for {}'.format(file) + e.desc)
