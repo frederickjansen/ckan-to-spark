0f68751d819d27fc1c1b0a9cacccaaa2cdf1e540
Frederick Jansen <frederick.jansen@gmail.com>
2018-04-30 13:22:17 -0400

    Overwrite existing Parquet files

--- ckan_to_spark/helpers/spark.py
+++ ckan_to_spark/helpers/spark.py
@@ -5,5 +5,8 @@ import os
 
 def csv_to_parquet(file):
     spark = SparkSession.builder.appName('csv-to-parquet').getOrCreate()
-    df = spark.read.option('header', 'true').option('inferschema', 'true').csv(file)
-    df.write.parquet(os.path.join(conf.FLAGS.output_dir, os.path.splitext(file)[0]) + '.parquet')
+    try:
+        df = spark.read.option('header', 'true').option('inferschema', 'true').csv(file)
+        df.write.mode('overwrite').parquet(os.path.join(conf.FLAGS.output_dir, os.path.splitext(file)[0]) + '.parquet')
+    except TypeError:
+        print('Spark Error: ' + file)
