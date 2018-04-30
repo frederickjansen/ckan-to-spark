c37c0448453d3b4b2f0ea0e5e366e7b38c337390
Frederick Jansen <frederick.jansen@gmail.com>
2018-04-30 02:38:00 -0400

    Auto infer headers

--- ckan_to_spark/helpers/spark.py
+++ ckan_to_spark/helpers/spark.py
@@ -5,5 +5,5 @@ import os
 
 def csv_to_parquet(file):
     spark = SparkSession.builder.appName('csv-to-parquet').getOrCreate()
-    df = spark.read.csv(file)
+    df = spark.read.option('header', 'true').option('inferschema', 'true').csv(file)
     df.write.parquet(os.path.join(conf.FLAGS.output_dir, os.path.splitext(file)[0]) + '.parquet')
